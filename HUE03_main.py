8a8e7d077a6e35cd8745616bb2d591a0b083e892
LuMaul <lucas.maul@hslu.ch>
2024-10-07 11:48:22 +0200

    added more clearity in line

--- HUE03_main.py
+++ HUE03_main.py
@@ -1,5 +1,7 @@
 from horloadist import SupportNode, Polygon, Stucture, LinSolve
 
+# Caution! Do not blindly trust this calculation, but always check
+# the plausibility of the results.
 
 def globalIy(dx, dy):
     if dx > dy:
@@ -20,12 +22,13 @@ w5 = SupportNode(5,   2.5,  7.5, globalIx(10,0.3), globalIy(10,0.3))
 w6 = SupportNode(6,   7.5,  0.0, globalIx(0.3, 5), globalIy(0.3, 5))
 w7 = SupportNode(7,  12.5, -5.0, globalIx(0.3, 5), globalIy(0.3, 5))
 
+
 plate = Polygon([[-12.5, -7.5], [-12.5, 7.5], [12.5, 7.5], [12.5, -7.5]])
 
-struc = Stucture(plate, [w1, w2, w3, w4, w5, w6, w7])
 
+struc = Stucture(plate, [w1, w2, w3, w4, w5, w6, w7])
 struc.printTable()
 
-shear = LinSolve(struc, 0, 1)
 
-shear.printTable()
+sol = LinSolve(struc, 0, 1)
+sol.printTable()
