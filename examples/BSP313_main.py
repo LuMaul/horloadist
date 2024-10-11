a4b850fc52e999cce5dd94b7f2bd787dd1199e29
LuMaul <lucas.maul@hslu.ch>
2024-10-11 13:33:55 +0200

    added stiffness module utilisation

--- examples/BSP313_main.py
+++ examples/BSP313_main.py
@@ -1,20 +1,31 @@
-from horloadist import SupportNode, Polygon, Stucture, LinSolve
+from horloadist import KX, KY, SupportNode, Polygon, Stucture, LinSolve
 
-def globalIy(dx, dy):
-    return dy*dx**3/12
 
-def globalIx(dx, dy):
-    return dx*dy**3/12
+kx1 = KX.constRectangular(0.25, 4)
+ky1 = KY.constRectangular(0.25, 4)
 
-w1 = SupportNode(1, -8, 3.0, globalIx(0.25, 4), globalIy(0.25, 4))
-w2 = SupportNode(2, -2, 2.0, globalIx(0.25, 6), globalIy(0.25, 6))
-w3 = SupportNode(3,  2,-5.0, globalIx(12,0.25), globalIy(12,0.25))
-w4 = SupportNode(4,  0, 5.0, globalIx(4, 0.25), globalIy(4 ,0.25))
+kx2 = KX.constRectangular(0.25, 6)
+ky2 = KY.constRectangular(0.25, 6)
+
+kx3 = KX.constRectangular(12, 0.25)
+ky3 = KY.constRectangular(12, 0.25)
+
+kx4 = KX.constRectangular(4, 0.25)
+ky4 = KY.constRectangular(4, 0.25)
+
+
+w1 = SupportNode(1, -8, 3.0, kx1, ky1)
+w2 = SupportNode(2, -2, 2.0, kx2, ky2)
+w3 = SupportNode(3,  2,-5.0, kx3, ky3)
+w4 = SupportNode(4,  0, 5.0, kx4, ky4)
+
 
 pl = Polygon([[8, 5], [8, -5], [-8, -5], [-8, 5]])
 
+
 struc = Stucture(pl, [w1, w2, w3, w4])
 struc.printTable()
+
 
 sol = LinSolve(struc, 1, -1)
 sol.printTable()
