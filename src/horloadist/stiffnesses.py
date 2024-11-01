4b15f1aeb445f00f72c254a4e057891874b2b90e
Lucas Maul <154314500+LuMaul@users.noreply.github.com>
2024-11-01 12:40:41 +0100

    added optional E_mod

--- src/horloadist/stiffnesses.py
+++ src/horloadist/stiffnesses.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 class KX:
     @staticmethod
-    def const(kx:float) -> float:
-        return kx
+    def const(kx:float, E_mod:float=1) -> float:
+        return E_mod * kx
 
     @staticmethod
-    def constRectangular(dx_glob:float, dy_glob:float) -> float:
-        return dy_glob * dx_glob**3 / 12 #EIy
+    def constRectangular(dx_glob:float, dy_glob:float, E_mod:float=1) -> float:
+        return E_mod * dy_glob * dx_glob**3 / 12 #EIy
     
     @staticmethod
     def from_csv(csv_path:str, momYColName:str, EIYColName:str) -> pd.DataFrame:
@@ -23,8 +23,8 @@ class KY:
         return kx
 
     @staticmethod
-    def constRectangular(dx_glob:float, dy_glob:float) -> float:
-        return dx_glob * dy_glob**3 / 12 #EIx
+    def constRectangular(dx_glob:float, dy_glob:float, E_mod:float=1) -> float:
+        return E_mod * dx_glob * dy_glob**3 / 12 #EIx
     
     @staticmethod
     def from_csv(csv_path:str, momXColName:str, EIXColName:str) -> pd.DataFrame:
